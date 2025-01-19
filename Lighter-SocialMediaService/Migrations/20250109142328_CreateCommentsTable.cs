9a8861854a1519476451dd519f2bd2f7c872fb94
Jakub Golec <kuba.golec.99.jr@gmail.com>
2025-01-19 23:15:43 +0100

    projekt final2 mig

---
+++ Lighter-SocialMediaService/Migrations/20250109142328_CreateCommentsTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lighter_SocialMediaService.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCommentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
---
+++ Lighter-SocialMediaService/Migrations/20250109145428_AddReplyToField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lighter_SocialMediaService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReplyToField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReplyTo",
+                table: "Comments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReplyTo",
+                table: "Comments");
+        }
+    }
+}
